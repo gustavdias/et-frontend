7dac94527453ac556d87bd72df4abb12effc4b5d
gustavdias <gustav.almd@gmail.com>
2020-07-31 12:00:37 +0300

    exercise-list update2

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -23,7 +23,7 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://et-exercise-tracker.herokuapp.com//users/')
+    axios.get('https://et-exercise-tracker.herokuapp.com/users/')
       .then(response => {
         if (response.data.length > 0) {
           this.setState({
